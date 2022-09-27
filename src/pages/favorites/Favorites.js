8bd90e806416eb581aadde232dccb4ce5adc8389
hhristovaa <hr.hristovaa@gmail.com>
2022-09-27 20:02:17 +0300

    fetch user favorites from firestore in the favorites pagw

--- src/pages/favorites/Favorites.js
+++ src/pages/favorites/Favorites.js
@@ -2,7 +2,7 @@ import classes from '../recipes/Recipes.module.scss';
 import FavoritesContext from '../../context/FavoritesContext';
 import RecipeItem from '../../components/Recipes/RecipeItem';
 import { useContext } from 'react';
-import { getDocs, doc, collection, query, where, orderBy, getDoc } from 'firebase/firestore';
+import { getDocs, doc, docs, collection, query, where, orderBy, getDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { db } from '../../firebase.config';
 import Spinner from '../../components/UI/Spinner';
@@ -15,7 +15,7 @@ const Favorites = props => {
     const favoritesCtx = useContext(FavoritesContext);
     const hasFavorites = favoritesCtx.recipes.length > 0;
 
-    const [favorite, setFavorite] = useState([]);
+    const [favorites, setFavorites] = useState([]);
     const [loading, setLoading] = useState(true);
 
     // const favoritesItems = (
@@ -31,57 +31,30 @@ const Favorites = props => {
     //     </section>
     // );
 
-    const navigate = useNavigate();
-    const params = useParams();
 
     useEffect(() => {
-        const fetchFavorites = async () => {
-            // try {
-                //get reference
-                const favoritesRef = doc(db, 'users', auth.currentUser.uid);
-              //  const q = query(favoritesRef, where('favorites', 'array-contains', 'id'), orderBy('timestamp', 'desc'));
-
-               const docSnap = await getDoc(favoritesRef);
-
-             //   const favoritesData = await getDocs(q);
-                let favorites = [];
-                console.log(docSnap.data());
-               
-
+        const fetchUserFavorites = async () => {
+            try {
+                const userRef = doc(db, 'users', auth.currentUser.uid)
+                const docSnap = await getDoc(userRef);
 
                 if (docSnap.exists()) {
-                    console.log('data here');
-                    let favData = docSnap.data().favorites;
-                    console.log(favData);
-                    // for (let fav of favData) {
-                    //     console.log(fav);
-                    // }
-                    console.log(favorite);
-
-                 favorites.push(favData.recipe);
-                    setFavorite(favData)
-
-                } else {
-                    console.log('no data here');
+                    let userFavs = docSnap?.data()?.favorites;
+                    setFavorites(userFavs);
                 }
 
-                console.log(favorite);
-             
-       
-
-               
-                // setFavorite(favorites);
                 setLoading(false);
-            // } catch (err) {
-            //     setLoading(false);
-            //     toast.error('An error occured while loading the favorites.');
-            // }
+            } catch (err) {
+                setLoading(false);
+                toast.error('An error occured while loading the favorites.');
+            }
 
 
         }
 
-        fetchFavorites();
+        fetchUserFavorites();
     }, [auth.currentUser.uid]);
+    console.log(favorites)
 
     if (loading) {
         return <Spinner />;
@@ -90,23 +63,24 @@ const Favorites = props => {
     return (
         <main>
             <h1 className={classes['g-title']}>Favorites </h1>
-            <h3 className={classes['recipes__counter']}>You have marked {favoritesCtx.recipes.length} recipes as favorites.</h3>
+            {/* <h3 className={classes['recipes__counter']}>You have marked {favoritesCtx.recipes.length} recipes as favorites.</h3> */}
+            <h3 className={classes['recipes__counter']}>You have marked {favorites.length} recipes as favorites.</h3>
             <section className={classes['recipes__container']}>
-            {!loading  && favorite?.length > 0 && favorite.map(recipe => {
-                return (
-                    <RecipeItem
-                        key={recipe?.id}
-                        recipe={recipe.data}
-                        
-                    />
-                );
-            })}
-               {/* <RecipeItem
+                {!loading && favorites?.length > 0 && favorites.map(recipe => {
+                    return (
+                        <RecipeItem
+                            key={recipe?.id}
+                            recipe={recipe}
+
+                        />
+                    );
+                })}
+                {/* <RecipeItem
                         key={favorite.id}
                         recipe={favorite.recipe}
                         
                     /> */}
-        </section> 
+            </section>
             {/* {favoritesItems} */}
         </main>
     );
