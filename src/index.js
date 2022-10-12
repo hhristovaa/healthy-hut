0bee1ccfd15146e9dea7d3368a758ed3f7d0b18c
hhristovaa <hr.hristovaa@gmail.com>
2022-10-12 21:49:11 +0300

    added and init react query refactor fetch diet types with react query

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, // 1 minute
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
   // </React.StrictMode>
 );
 
--- src/pages/recipes/Diet.js
+++ src/pages/recipes/Diet.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { useQuery, useMutation, useQueryClient, queryCache } from 'react-query';
 
 import Spinner from '../../components/UI/Spinner';
 import RecipeItem from '../../components/Recipes/RecipeItem';
@@ -11,23 +12,54 @@ import classes from './Recipes.module.scss';
 
 const Diet = () => {
     let params = useParams();
+    const queryClient = useQueryClient();
+    const apiKey = 'a3577636ccd3420a92a088027e661830';
+    // const apiKey = 'b44514ae9c644024a55ec4e856cf0fd2';
+    const BASE_URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&number=10&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true&limitLicense=true`;
+    
+    const getDiet = async (params) => await client.get(`&diet=${params.type}`);
+    // const getDiet = async () => await client.get(BASE_URL, {params: {
+    // 'diet': 'vegan'
+    // }
+    // });
+    // const getDietApi = useApi(getDiet);
+    const { isLoading, isError, error, data } = useQuery(['diet', params.type], () => getDiet(params));
 
-    const getDiet = (name) => client.get(`&diet=${name}`);
-    const getDietApi = useApi(getDiet);
+    console.log(data);
+    console.log(params.type);
 
-    useEffect(() => {
-        getDietApi.request(params.type)
-    }, [params.type]);
+    // useEffect(() => {
+    //     getDietApi.request(params.type)
+    // }, [params.type]);
 
     let title = capitalizeFirstLetter(params.type);
 
+    let content;
+
+    if (isLoading) {
+        <Spinner />
+    } else if (isError) {
+        toast.error(error.message)
+    } else if (data.status === 402) {
+        toast.warning('No recipes are available right now. Please try again later.')
+    }
+    else {
+        content = data;
+    }
+
     return (
         <main>
-            {getDietApi.loading && <Spinner />}
-            {getDietApi.error && toast.error(getDietApi.error)}
+            {/* {getDietApi.loading && <Spinner />}
+            {getDietApi.error && toast.error(getDietApi.error)} */}
             <h1 className={classes['g-title']}>{title}</h1>
             <section className={classes['recipes__container']}>
-                {getDietApi.data?.results.map((item) => {
+                {/* {getDietApi.data?.results.map((item) => {
+                    return (
+                        <RecipeItem key={item.id} recipe={item} />
+                    )
+                })} */}
+
+                {content?.data?.results.map((item) => {
                     return (
                         <RecipeItem key={item.id} recipe={item} />
                     )
